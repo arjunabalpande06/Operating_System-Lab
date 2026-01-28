f50479b4678eabbe2be35cc572539506a6b7e96d
arjunabalpande06 <arjunabalpande@gmail.com>
2026-01-28 10:41:37 +0530

    Add files via upload

---
+++ Experiment 4/exp4_pid.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/wait.h>
+
+int main()
+{
+pid_t pid;
+printf("Parent Process Started\n");
+printf("Parent PID : %d\n",getpid());
+pid = fork();
+if(pid < 0){
+printf("Process Creation Failed\n");
+}
+else if(pid == 0)
+{
+printf("\nChild Process\n");
+printf("Child PID : %d\n",getpid());
+printf("Parent PID : %d\n",getppid());
+execl("/bin/ls","ls","-1",NULL);
+printf("Exxec failed\n");
+}
+else{
+wait(NULL);
+printf("\nChild process comepleted\n");
+printf("Parent process existing\n");
+}
+return 0;
+}
+
