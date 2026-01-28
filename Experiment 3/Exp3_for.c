65784b5d2b1c320a7e6c27142e1ee780df86504e
arjunabalpande06 <arjunabalpande@gmail.com>
2026-01-28 10:37:54 +0530

    Add files via upload

---
+++ Experiment 3/Exp3_for.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int i = 0;
+int n = 11;
+for(i; i < n; i++){
+printf("%d\n",i);
+}
+return 0;
+}
---
+++ Experiment 3/exp3_while.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int i = 1;
+int n = 5;
+while(i <= 5)
+{
+printf("%d\n",i);
+i++;
+}
+return 0;
+}
